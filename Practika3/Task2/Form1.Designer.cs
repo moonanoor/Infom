5cdc0927160b75037dc7c4fcd5134e5c48224c33
Oleg Ladnyi <oleg.ladn@gmail.com>
2021-03-25 19:29:49 +0300

    Ready

---
+++ Practika3/Task2/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите х: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(229, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 135);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(657, 200);
+            this.textBox4.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(688, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Расчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.button1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button1_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Разветвляющийся алгоритм";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Practika3/Task2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public double x_max(double x)
+        {
+            x = 1;
+            return x;
+        }
+        public double x_mid(double x)
+        {
+            x = x * 2 - 1;
+            return x;
+        }
+        public double x_min(double x)
+        {
+            x = Math.Abs(1 - 3 * x);
+            return x;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Получение исходных данных из TextBox
+            double x = Convert.ToDouble(textBox1.Text);
+            double y;
+            // Ввод исходных данных в окно результатов
+            textBox4.Text = "Результаты работы программы ст. Ладный О.К. " + Environment.NewLine;
+            textBox4.Text += "При x = " + textBox1.Text + Environment.NewLine;
+            //  textBox4.Text += "При Y = " + textBox2.Text + Environment.NewLine;
+            //Проверка ООФ
+            int n = 0;
+            if ((0 <= x) & (x <= 10))
+            {
+                n = 1;
+            }
+            else if (x < 0)
+            {
+                n = 2;
+            }
+            switch (n)
+            {
+                case 0:
+                    y = x_max(x);
+                    textBox4.Text += "y = " + Convert.ToString(y) + Environment.NewLine;
+                    break;
+                case 1:
+                    y = x_mid(x);
+                    textBox4.Text += "y = " + Convert.ToString(y) + Environment.NewLine;
+                    break;
+                case 2:
+                    y = x_min(x);
+                    textBox4.Text += "y = " + Convert.ToString(y) + Environment.NewLine;
+                    break;
+                default:
+                    textBox4.Text += "Решение не найдено" + Environment.NewLine;
+                    break;
+            }
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void button1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) button1_Click(sender, e);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) button1_Click(sender, e);
+
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) button1_Click(sender, e);
+        }
+    }
+}
