e314fddb40cf9da14d5bd8619605cef3d40841b9
Oleg Ladnyi <oleg.ladn@gmail.com>
2021-03-29 15:36:27 +0300

    Ready

---
+++ Praktica5/Task1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Посчитать";
+            button2.Text = "Добавить строку";
+            button3.Text = "Удалить строку";
+            label1.Text = "Количество букв 'а': 0";
+            listBox1.Items.Add("Привет");
+            listBox1.Items.Add("аааа");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Получаем номер выделенной строки
+            int index = listBox1.SelectedIndex;
+            // Считываем строку в перменную str
+            string str = (string)listBox1.Items[index];
+            // Узнаем количество символов в строке
+            int len = str.Length;
+            // Считаем, что количество пробелов равно 0
+            int count = 0;
+            // Устанавливаем счетчик символов в 0
+            int i = 0;
+            str.ToLower();
+            //Организуем цикл перебора всех символов в строке
+            while (i < len)
+            {
+                // Если нашли пробел, то увеличиваем
+                // счетчик пробелов на 1
+                if (str[i] == 'а')
+                    count++;
+                i++;
+            }
+            label1.Text = "Количество букв 'а': " + count.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+                listBox1.Items.Add(Convert.ToString(textBox1.Text));
+            
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if ((textBox2.Text != "") & (Convert.ToInt32(textBox2.Text) > 0))
+                listBox1.Items.RemoveAt(Convert.ToInt32(textBox2.Text) - 1);
+        }
+    }
+}
---
+++ Praktika4/task1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Double x = Convert.ToDouble(textBox2.Text);
+            textBox1.Text = "Результаты работы программы ст. Ладный О.К. " + Environment.NewLine;
+            double current = 1;
+            int i = 0;
+            long F = 1;
+            double s = 0;
+            while (Math.Abs(current) >= 0.0001)
+            {
+                F *= ++i;
+                current = (x + 1) / (F * 2.0);
+                s += current;
+            }
+            textBox1.Text += "При eps = " + textBox2.Text + Environment.NewLine;
+            textBox1.Text += "Расчет суммы ряда S = " + Convert.ToString(s) + Environment.NewLine;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                button1_Click(sender, e);
+        }
+    }
+}
